e5b067b0d3c59210460fe1ab118b676594f8a5ec
Henry AT <yiperu@gmail.com>
2014-07-22 14:17:10 -0400

    Ejemplo Inicial, donde aprendi el uso de variables y constantes

---
+++ 01pryHolaMundo/MyPlaygroundHolaMundo.playground/section-1.swift
@@ -0,0 +1,39 @@
+// Playground - noun: a place where people can play
+// En este ejemplo vemos como reacciona
+
+import Cocoa
+
+var str = "Hello, Mundo"
+// Podemos indicarle el tipo de dato a utiliza
+var cadena:String = "Hola Universo"
+var entero:Int = 30
+// Las variables se declaran con var
+var nombre = "Henry"
+var apellido = "Ambicho"
+// Podemos contatenar de 2 formas + o \()
+// En la 2da forma van todos tipos de variables
+var nombreCompleto1 = nombre + apellido
+var nombreCompleto2 = "\(nombre) \(apellido)"
+// Podemos declarar de frente diferentes tipos de datos
+// int
+var valorEntero = 10
+// float
+var valorFlotante = 0.34964255345
+// booleano
+var valorBooleano = true
+// Ejemplo de como utilizariamo el caso de llamar API
+//http://www.lawebapi.com/index.php?variable1=\(data1)&variable2=\(data2)
+// Tambien podemos calcualr operaciones dentro del \()
+var tres = 3, siete = 7
+var resultado = "La suma de 3 + 7 es = \(tres + siete) unidades"
+// !!Importante , espacios: x=3 es diferente de x = 3
+
+// Las constantes se declaran con let, en caso de declarar API, etc.
+let puntajeMax = 999
+
+
+
+
+
+
+
