57ebd7bab47f20c4354967d8e3d75756caabd684
Henry AT <yiperu@gmail.com>
2014-07-23 00:41:15 -0400

    Configuracion Inicial del Proyecto Adicion de Notas

---
+++ 09ProyectoNotas/PryNotas/PryNotas/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PryNotas
+//
+//  Created by Henry Antonio Ambicho Trujillo on 7/23/14.
+//  Copyright (c) 2014 Apps4s. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
