5cab065f363ddf5c4aae2881659acd33ad81eccd
Henry AT <yiperu@gmail.com>
2014-08-03 16:16:36 -0400

    Ahora podemos utilizar los gestos para mover la caja y desde ahi, se caeria

--- 10PryCajaDinamica/PryCajaDinamica/PryCajaDinamica/Main.swift
+++ 10PryCajaDinamica/PryCajaDinamica/PryCajaDinamica/Main.swift
@@ -20,6 +20,9 @@ class Main: UIViewController {
     // Variable para la base donde hara la colision
     var colision: UICollisionBehavior?
     
+    // declaramos la variable para trabajar con los gestos y lo agregaremos a nuestra caja
+    var panGesture: UIPanGestureRecognizer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -46,11 +49,44 @@ class Main: UIViewController {
         self.colision!.translatesReferenceBoundsIntoBoundary = true;
         
         
-        // Aqui vamos
+        // Aqui vamos (Esto hara que la caja caiga y choque en el suelo)
         // Esto le da el comportamiento de gravedad a la caja azul
         self.animador!.addBehavior(self.gravedad);
         // Esto le da el comportamientos de colicion a la base
         self.animador!.addBehavior(self.colision);
         
+        
+        // Instanciamos el detector de gestos y lo adicionamos a nuestra caja
+        self.panGesture = UIPanGestureRecognizer(target: self, action:"panning:");
+        self.cajaAzul!.addGestureRecognizer(panGesture);
+        
     }
+    
+    func panning(pan: UIPanGestureRecognizer){
+        println("Nuestra caja es panning ");
+        var locacion = pan.locationInView(self.view);
+        var touchLocacion = pan.locationInView(self.cajaAzul);
+        
+        if pan.state == UIGestureRecognizerState.Began {
+            // Hacer las configuraciones iniciales aqui:
+            
+            // Remover todos los comportamientos adjuntados al animador por ahora
+            self.animador!.removeAllBehaviors();
+            
+            // Estableceremos el centro de la caja para el valor de la locacion
+            self.cajaAzul!.center = locacion;
+            
+        } else if pan.state == UIGestureRecognizerState.Changed {
+            self.cajaAzul!.center = locacion;
+        } else if pan.state == UIGestureRecognizerState.Ended {
+            // Manejo de lo que deveira pasar si la caja es relanzada
+            self.animador!.addBehavior(self.gravedad);
+            self.animador!.addBehavior(self.colision);
+        }
+    }
+    
+    
+    
+    
+    
 }
