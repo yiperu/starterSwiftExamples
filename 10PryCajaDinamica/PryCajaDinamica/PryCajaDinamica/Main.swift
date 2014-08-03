1877b71660e3e7e43f45dd44699e5377d09eca84
Henry AT <yiperu@gmail.com>
2014-08-03 14:48:14 -0400

    Creacion del donde de una caja Dinamica, donde usaremos UIKit y gestos

---
+++ 10PryCajaDinamica/PryCajaDinamica/PryCajaDinamica/Main.swift
@@ -0,0 +1,13 @@
+//
+//  Main.swift
+//  PryCajaDinamica
+//
+//  Created by Henry Antonio Ambicho Trujillo on 8/3/14.
+//  Copyright (c) 2014 Apps4s. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+class Main: UIViewController {
+
+}
