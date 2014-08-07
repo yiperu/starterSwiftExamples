7194eba3789fd2c8fc6899c1d2f4b7bba6168b94
Henry AT <yiperu@gmail.com>
2014-08-07 02:29:08 -0400

    implementacion para ver al ave mover las alas

--- 11PryFlapyBird/FlappyBird/FlappyBird/GameScene.swift
+++ 11PryFlapyBird/FlappyBird/FlappyBird/GameScene.swift
@@ -9,34 +9,34 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var bird = SKSpriteNode();
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        var birdTexture1 = SKTexture(imageNamed:"Bird1");
+        birdTexture1.filteringMode = SKTextureFilteringMode.Nearest
+        var birdTexture2 = SKTexture(imageNamed:"Bird2");
+        birdTexture2.filteringMode = SKTextureFilteringMode.Nearest
+        
+        var animation = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.2)
+        var flap = SKAction.repeatActionForever(animation)
+        
+        bird = SKSpriteNode(texture:birdTexture1)
+        bird.position = CGPoint(x:self.frame.size.width / 2.0, y:CGRectGetMidY(self.frame))
+        bird.runAction(flap)
+        
+        self.addChild(bird)
+
+        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
