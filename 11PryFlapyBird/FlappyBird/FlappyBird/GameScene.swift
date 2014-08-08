638bdfd114149d5fc148bc0bb47ad6077f7f1b57
Henry AT <yiperu@gmail.com>
2014-08-08 00:43:59 -0400

    Terminamos con el juego por ahora, implementamos los score, para que se vizualizen en el fondo

--- 11PryFlapyBird/FlappyBird/FlappyBird/GameScene.swift
+++ 11PryFlapyBird/FlappyBird/FlappyBird/GameScene.swift
@@ -13,7 +13,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var bird = SKSpriteNode();
     var skyColor = SKColor();
     // varible para el pipe
-    var verticalPipeGap = 130.0
+    var verticalPipeGap = 230.0
     //---
     var pipeTexture1 = SKTexture();
     var pipeTexture2 = SKTexture();
@@ -22,11 +22,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let birdCategory:UInt32 = 1 << 0
     let wordCategory:UInt32 = 1 << 1
     let pipeCategory:UInt32 = 1 << 2
+    let scoreCategory: UInt32 = 1 << 3
     
     var moving = SKNode()
     // Trabajaremos en el restar animation
     var canRestart = false
     var pipes = SKNode()
+    
+    var scorelabelNode = SKLabelNode()
+    var score = NSInteger()
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -135,6 +139,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         var spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
         self.runAction(spawnThenDelayForever)
         
+        score = 0
+        scorelabelNode.fontName = "Helvetica-Bold"
+        scorelabelNode.position = CGPointMake(CGRectGetMidX(self.frame), self.frame.size.height / 6)
+        scorelabelNode.fontSize = 600
+        scorelabelNode.alpha = 0.2
+        scorelabelNode.zPosition = -30
+        scorelabelNode.text = "\(score)"
+        self.addChild(scorelabelNode)
+        
     }
     
     func spawnPipes(){
@@ -162,6 +175,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         pipe2.physicsBody.contactTestBitMask = birdCategory
         pipePair.addChild(pipe2)
         
+        var contactNode = SKNode()
+        contactNode.position = CGPointMake(pipe1.size.width + bird.size.width / 2, CGRectGetMidY(self.frame))
+        contactNode.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(pipe1.size.width, self.frame.size.height))
+        contactNode.physicsBody.dynamic = false
+        contactNode.physicsBody.categoryBitMask = scoreCategory
+        contactNode.physicsBody.contactTestBitMask = birdCategory
+        pipePair.addChild(contactNode)
+        
         // pipePair.runAction(moveAndRemovePipes) // Creo que esto esta mal
         pipePair.runAction(movePipesAndRemove)
         //self.addChild(pipePair)
@@ -180,6 +201,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         pipes.removeAllChildren()
         canRestart = false
         moving.speed = 1
+        
+        score = 0;
+        scorelabelNode.text = "/(score)"
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -197,6 +221,70 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
    
+    
+    // Ahora implementamos el contex notificacion delegate
+    func didBeginContact (contact: SKPhysicsContact!){
+        
+        if(moving.speed > 0){
+            
+            if ( (contact.bodyA.categoryBitMask & scoreCategory ) == scoreCategory || (contact.bodyB.categoryBitMask & scoreCategory ) == scoreCategory ){
+                score++;
+                scorelabelNode.text = "\(score)"
+            } else {
+            
+                moving.speed = 0
+                
+                bird.physicsBody.collisionBitMask = wordCategory
+                
+                var rotateBird = SKAction.rotateByAngle(0.01, duration: 0.03)
+                var stopBird = SKAction.runBlock({() in self.killBirdSpeed()})
+                var birdSequence = SKAction.sequence([rotateBird, stopBird])
+                bird.runAction(birdSequence)
+                
+                self.removeActionForKey("flash")
+                var turnBackgroundRed = SKAction.runBlock({() in self.setBackgroundRed()})
+                var wait = SKAction.waitForDuration(0.05)
+                var turnBackgroundWhite = SKAction.runBlock({() in self.setBackgroundWhite()})
+                var turnBackgroundColorSky = SKAction.runBlock({() in self.setBackgroundColorSky()})
+                
+                // Implementamos el efecto del explosion de fondo con luces
+                var sequenceOfActions = SKAction.sequence([turnBackgroundRed, wait, turnBackgroundWhite, wait, turnBackgroundColorSky])
+                var repeatSequence = SKAction.repeatAction(sequenceOfActions, count: 4)
+                // Ingresamos aqui el efecto restart
+                var canRestartAction = SKAction.runBlock({() in self.letItRestart()})
+                var groupOfActions = SKAction.group([repeatSequence, canRestartAction])
+                
+                //self.runAction(repeatSequence, withKey: "flash") // comenta esto ara cambiar por el efecto restart
+                
+                self.runAction(groupOfActions, withKey: "flash")
+            }
+            
+
+        }
+    
+    }
+    
+    func killBirdSpeed(){
+        bird.speed = 0
+        
+    }
+    
+    func letItRestart(){
+        canRestart = true
+    }
+    
+    func setBackgroundRed(){
+        self.backgroundColor = UIColor.redColor()
+    }
+    
+    func setBackgroundWhite(){
+        self.backgroundColor = UIColor.whiteColor()
+    }
+    
+    func setBackgroundColorSky(){
+        self.backgroundColor = SKColor(red:113.0/255.0, green: 197.0/255.0, blue:207.0/255.0, alpha: 1.0)
+    }
+    
     // Implementaremos la rotacion del ave cuando se eleve, para esto tenemos que implementar una funcion que devuelva el max o min valor
     
     func clamp (min: CGFloat, max: CGFloat, value: CGFloat) -> CGFloat {
@@ -216,78 +304,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if moving.speed > 0 {
             bird.zRotation = self.clamp(-1, max: 0.5, value: bird.physicsBody.velocity.dy * (bird.physicsBody.velocity.dy < 0 ? 0.003 : 0.001))
         }
-        
-        
     }
-    
-    // Ahora implementamos el contex notificacion delegate
-    func didBeginContact (contact: SKPhysicsContact!){
-        
-        if(moving.speed > 0){
-            moving.speed = 0
-            
-            bird.physicsBody.collisionBitMask = wordCategory
-            
-            var rotateBird = SKAction.rotateByAngle(0.01, duration: 0.03)
-            var stopBird = SKAction.runBlock({() in self.killBirdSpeed()})
-            var birdSequence = SKAction.sequence([rotateBird, stopBird])
-            bird.runAction(birdSequence)
-            
-            self.removeActionForKey("flash")
-            var turnBackgroundRed = SKAction.runBlock({() in self.setBackgroundRed()})
-            var wait = SKAction.waitForDuration(0.05)
-            var turnBackgroundWhite = SKAction.runBlock({() in self.setBackgroundWhite()})
-            var turnBackgroundColorSky = SKAction.runBlock({() in self.setBackgroundColorSky()})
-            
-            // Implementamos el efecto del explosion de fondo con luces
-            var sequenceOfActions = SKAction.sequence([turnBackgroundRed, wait, turnBackgroundWhite, wait, turnBackgroundColorSky])
-            var repeatSequence = SKAction.repeatAction(sequenceOfActions, count: 4)
-            // Ingresamos aqui el efecto restart
-            var canRestartAction = SKAction.runBlock({() in self.letItRestart()})
-            var groupOfActions = SKAction.group([repeatSequence, canRestartAction])
-            
-            //self.runAction(repeatSequence, withKey: "flash") // comenta esto ara cambiar por el efecto restart
-            
-            self.runAction(groupOfActions, withKey: "flash")
-        }
-    
-    }
-    
-    func killBirdSpeed(){
-        bird.speed = 0
-        
-    }
-    
-    func letItRestart(){
-        
-    }
-    
-    func setBackgroundRed(){
-        self.backgroundColor = UIColor.redColor()
-    }
-    
-    func setBackgroundWhite(){
-        self.backgroundColor = UIColor.whiteColor()
-    }
-    
-    func setBackgroundColorSky(){
-        self.backgroundColor = SKColor(red:113.0/255.0, green: 197.0/255.0, blue:207.0/255.0, alpha: 1.0)
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
